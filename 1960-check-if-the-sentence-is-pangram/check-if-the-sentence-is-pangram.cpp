f5355adc48409a927293e79ecf40c4a7f3d90e00
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-01-08 09:33:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (11.19%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_map<char,int>mp;
+        for(auto c:sentence){
+            mp[c]++;
+        }
+        return mp.size()==26;
+    }
+};
