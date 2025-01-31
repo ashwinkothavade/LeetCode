f4f436116658d2d0e331e0414081c41c1535cff8
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-01-31 13:23:55 +0530

    Time: 297 ms (35.29%) | Memory: 166.9 MB (27.13%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,66 +1,81 @@
 class Solution {
-public:
-    int n, N;
-    inline bool inside(int i, int j){
-        return 0<=i && i<n && 0<=j && j<n;
-    }
-    inline int idx(int i,  int j){
-        return i*n+j;
-    }
-    const int d[5]={0, 1, 0, -1, 0};
-    vector<int> viz;// -1 for unvisited, other for component number
 
-    int dfs(int i, int j, int comp, vector<vector<int>>& grid) {
-        viz[idx(i, j)] = comp;
-        int cnt = 1;
-        for (int a=0; a < 4; a++) {
-            int r=i+d[a], s=j+d[a+1];
-            if (inside(r, s) && grid[r][s]==1 && viz[idx(r, s)]==-1)
-                cnt+= dfs(r, s, comp, grid);
-        }
-        return cnt;
+private:
+    bool isValid(int r, int c, int n){
+        if(r>=n || c>=n || r<0 || c<0) return false;
+        return true;
     }
+
+public:
+
+    class Ds{
+        public:
+            vector<int> par,size;
+            Ds(int n){
+                for(int i=0;i<=n;i++) par.push_back(i);
+                size.resize(n+1,1);
+            }
+            int upar(int a){
+                if(a==par[a]) return a;
+                return par[a] = upar(par[a]);
+            }
+            void unionS(int a, int b){
+                int ap = upar(a), bp = upar(b);
+                if(ap==bp) return;
+                int as = size[ap], bs = size[bp];
+                if(as<bs){
+                    par[ap] = bp;
+                    size[bp]+=as;
+                } else{
+                    par[bp] = ap;
+                    size[ap]+=bs;
+                }
+            }
+    };
 
     int largestIsland(vector<vector<int>>& grid) {
-        n = grid.size();
-        N = n*n;
-        viz.assign(N, -1);
-        
-        int maxSz=1, comp=0;
-        vector<int> Size; // size for component[comp]  
-
-        // Find components
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j]==1 && viz[idx(i, j)]==-1) {
-                    int cnt=dfs(i, j, comp++, grid);
-                    Size.push_back(cnt);
-                    maxSz=max(maxSz, cnt);
-                }
-            }
-        }
-
-        // 2nd loop
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if (grid[i][j]==0){
-                    int component[4]={N, N, N, N};
-                    int cz=0;
-                    for( int a=0; a<4; a++){
-                        const int r=i+d[a], s=j+d[a+1];
-                        if (inside(r, s) && grid[r][s]!=0)
-                            component[cz++]=viz[idx(r, s)];
+        int n = grid.size();
+        Ds ds(n*n);
+        vector<int> dx = {-1,1,0,0};
+        vector<int> dy = {0,0,1,-1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int node = i*n + j;
+                if(grid[i][j]==1){
+                    for(int ind=0;ind<4;ind++){
+                        int newx = i + dx[ind];
+                        int newy = j + dy[ind];
+                        if(isValid(newx,newy,n) && grid[newx][newy]==1){
+                            int newNode = (newx)*n + newy;
+                            ds.unionS(node,newNode);
+                        }
                     }
-                    if (cz==0) continue;
-                    sort(component, component+cz);
-                    int sz=1+Size[component[0]];
-                    for (int k=1; k<cz; k++)
-                        if (component[k]!=component[k-1]) 
-                            sz+=Size[component[k]];
-                    maxSz=max(maxSz, sz);
                 }
             }
         }
-        return maxSz;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    unordered_set<int> se;
+                    for(int ind=0;ind<4;ind++){
+                        int newx = i + dx[ind];
+                        int newy = j + dy[ind];
+                        if(isValid(newx,newy,n) && grid[newx][newy]==1){
+                            se.insert(ds.upar(newx*n+newy));
+                        }
+                    }
+                    int temp = 1;
+                    for(auto i:se){
+                        temp+=ds.size[i];
+                    }
+                    ans = max(ans,temp);
+                }
+            }
+        } 
+        for(int i=0;i<n*n;i++){
+            ans = max(ans,ds.size[ds.upar(i)]);
+        }
+        return ans;
     }
 };
