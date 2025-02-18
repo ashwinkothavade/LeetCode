44734df0c63edc4974d1105bc556aeced244d04a
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-02-18 11:48:59 +0530

    Time: 55 ms (6.29%) | Memory: 8.3 MB (42.39%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
+//the time complexity of this code is O(N!)as it is trying all the possible combinations and giving the lexigraphically smallest number
+//i will check all the possible permutations of the string size n+1 and return the permutation which satisfies the given condition
     bool check(string s,string pattern){
-    int n=s.length();
-    int m=pattern.length();
-    for(int i=0;i<n;i++){
-        if(pattern[i]=='I'){
-            if(s[i]>s[i+1]){
-                return false;
-            }
-        }else{
-            if(s[i]<s[i+1]){
-                return false;
+        int n=s.length();
+        int m=pattern.length();
+        for(int i=0;i<n;i++){
+            if(pattern[i]=='I'){
+                if(s[i]>s[i+1]){
+                    return false;
+                }
+            }else{
+                if(s[i]<s[i+1]){
+                    return false;
+                }
             }
         }
+        return true;
     }
-    return true;
-}
-string smallestNumber(string s){
-    int n=s.size();
-    string result;
-    for(int i=1;i<=n+1;i++){
-        result+=to_string(i);
+    string smallestNumber(string s){
+        int n=s.size();
+        string result;
+        for(int i=1;i<=n+1;i++){
+            result+=to_string(i);
+        }
+        while(!check(result,s)){
+            next_permutation(result.begin(),result.end());
+        }
+        return result;
     }
-    while(!check(result,s)){
-        next_permutation(result.begin(),result.end());
-    }
-    return result;
-}
 };
