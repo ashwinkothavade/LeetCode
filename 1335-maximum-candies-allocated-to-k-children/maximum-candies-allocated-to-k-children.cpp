69d8e49299923d5d4b560f7e8aafd4f775bde540
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-03-14 19:03:03 +0530

    Time: 22 ms (77.72%) | Memory: 88.3 MB (41.19%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,30 +1,28 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
 class Solution {
-public:
-    bool valid_bin_sea(vector<int>& candies, long long k, long long search){
-        long long ans = 0;
-        if(!search) return false;
-        int n = candies.size();
-        if(search > 1e7) search = 1e7+1;
-        for(int i = 0;i < n; i++){
-            ans += candies[i]/search;
+    bool pred(const std::vector<int>& candies, long long k, long long mid) {
+        long long count = 0;
+        for (int candy : candies) {
+            count += candy / mid; 
         }
-        return ans >= k;
+        return count >= k;
     }
-    int maximumCandies(vector<int>& candies, long long k) {
-        int n = candies.size();
-        int ans = 0;
-        long long low = 0, hi = 1e18;
-        long long mid = low + (hi-low)/2;
-        while(low < hi){
-            mid = low + (hi-low)/2;
-            if(valid_bin_sea(candies,k,mid)){
-                ans = mid;
-                low = mid+1;
-            }else{
-                hi = mid;
+public:
+    int maximumCandies(std::vector<int>& candies, long long k) {
+        long long low = 1; 
+        long long high = *max_element(candies.begin(), candies.end());
+        
+        while (low <= high) {
+            long long mid = low + (high - low) / 2; 
+            if (pred(candies, k, mid)) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
             }
         }
-        mid = low + (hi-low)/2;
-        return ans;
+        return high; 
     }
 };
