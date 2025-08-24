cd73108b79a3a74baa2094b7a846f58d60994733
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-08-25 00:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (52.58%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -8,6 +8,15 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return func(n,dp);
+        // return func(n,dp);
+        //tabulation
+        dp[0]=0;
+        dp[1]=1;
+        if(n>1)
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
