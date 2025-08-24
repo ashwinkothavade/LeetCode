4f8a731060ba6ef47ff8fced7a2d376c7763db2e
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-08-25 01:01:41 +0530

    Time: 379 ms (19.75%) | Memory: 8.6 MB (81.34%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(s, i, j)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
