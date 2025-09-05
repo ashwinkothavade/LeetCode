15dac6a81ee6f9cd29c9877e7661ec1b2b4aa577
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-09-05 19:22:17 +0530

    Time: 6 ms (53.99%) | Memory: 19.3 MB (74.94%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(n);
+        for(auto &it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> indegree(n, 0);
+        for(auto &it : adj){
+            for(auto i : it){
+                indegree[i]++;
+            }
+        }
+
+        vector<int> ans;
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+
+        if(ans.size() == n) return true;  // valid topological order
+        return false; // cycle exists
+    }
+};
