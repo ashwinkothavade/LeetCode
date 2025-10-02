51e268b8a21ea37b0b0a3a2a38fbadaa321a7161
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2025-10-02 11:44:27 +0530

    Time: 92 ms (5.81%) | Memory: 80.3 MB (5.14%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,43 +1,37 @@
 class Solution {
-    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& vis, int i, int j) {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int,int>> q;
-        q.push({i, j});
-        vis[i][j] = true;
-
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
-
-        while (!q.empty()) {
-            auto it = q.front();
+    void bfs(vector<vector<char>>&grid,vector<vector<bool>>&vis,int i,int j,int n,int m){
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        vis[i][j]=true;
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty()){
+            auto it=q.front();
             q.pop();
-            int row = it.first;
-            int col = it.second;
-
-            for (int k = 0; k < 4; k++) {
-                int nrow = row + delrow[k];
-                int ncol = col + delcol[k];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    !vis[nrow][ncol] && grid[nrow][ncol] == '1') {
-                    q.push({nrow, ncol});
-                    vis[nrow][ncol] = true; 
+            int row=it.first;
+            int col=it.second;
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0&&ncol>=0&&nrow<n&&ncol<m&&!vis[nrow][ncol]&&grid[nrow][ncol]=='1'){
+                    vis[nrow][ncol]=true;
+                    bfs(grid,vis,nrow,ncol,n,m);
                 }
             }
         }
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j]&&grid[i][j]=='1'){
                     cnt++;
-                    bfs(grid, vis, i, j);
+                    bfs(grid,vis,i,j,n,m);
+                    // vis[i][j]=true;
                 }
             }
         }
