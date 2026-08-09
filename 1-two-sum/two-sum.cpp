bda1e411ee1593724f8eb44c2569ae2d5fc853b4
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2026-08-09 17:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (40.94%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,10 +5,10 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             if(mp.find(target-nums[i])!=mp.end()){
-                return {i,mp[target-nums[i]]};
+                return{i,mp[target-nums[i]]};
             }
             mp[nums[i]]=i;
         }
-        return {-1,-1}
-;    }
+        return {-1,-1};
+    }
 };
