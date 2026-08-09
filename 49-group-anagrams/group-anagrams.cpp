907b4bc32702a9c24a52b6a5c803015b9c12a98e
ASHWIN MILIND KOTHAWADE <119505514+ashwinkothavade@users.noreply.github.com>
2026-08-09 17:58:30 +0530

    Time: 15 ms (69.17%) | Memory: 26.9 MB (24.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<string> str2=strs;
+        int n=strs.size();
+        for(int i=0;i<n;i++){
+            sort(str2[i].begin(),str2[i].end());
+        }
+        // for(auto it:str2){
+        //     cout<<it<<endl;
+        // }
         unordered_map<string,vector<string>>mp;
-        for(auto s:strs){
-            string t=s;
-            sort(t.begin(),t.end());
-            mp[t].push_back(s);
+        for(int i=0;i<n;i++){
+            if(mp.find(str2[i])!=mp.end()){
+                mp[str2[i]].push_back(strs[i]);
+            }else{
+                mp[str2[i]].push_back(strs[i]);
+            }
         }
         vector<vector<string>>ans;
         for(auto it:mp){
             ans.push_back(it.second);
         }
         return ans;
+        return {};
+
     }
 };
